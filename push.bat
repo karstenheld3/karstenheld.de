6867d75c3797e01a781fcab19cdb8bfde82ebbf4
Karsten Held <karstenheld3@gmail.com>
2019-11-29 19:37:10 +0100

    removed line in index.html

--- push.bat
+++ push.bat
@@ -1,5 +1,12 @@
+rem git add -u = looks at all the already tracked files and stages the changes to those files if they are different or if they have been removed. It does not add any new files.
 git add -u
+
+rem git add . = add all new files
+
 git add .
+
+
 git commit -m "new pushed version"
+
 git push https://github.com/karstenheld3/karstenheld.de.git master
 pause
